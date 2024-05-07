d8bb0e80a0afb2c6c3aa6a3b0d7c1ca19254912f
waarrdii <waarrdii@gmail.com>
2024-05-07 20:38:37 +0700

    title

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="flex flex-col h-screen">
-            <div class="flex h-28 text-gray-50 bg-cyan-500 w-44">
+            <div class="flex h-12 text-gray-50 bg-cyan-500 w-40">
                 <Logo/>
                 <span>Dashboard</span>
             </div>
-            <div class="flex-1 bg-slate-100">
-                <div class=" w-44 h-24 flex -translate-y-5">
-                    <div class="h-40 -translate-y-20 -translate-x-10 w-40 bg-cyan-500  rounded-full">1</div>
-                    <div class="">2</div>
+            <div class="flex-1 bg-slate-100 ">
+                <div class="h-14 relative overflow-hidden">
+                    <div class="bg-slate-100">
+                        <div class="absolute -left-[10px] -top-12 h-24 w-24 rounded-full bg-cyan-500"></div>
+                    </div>
+                    <div class="bg-cyan-500 absolute -right-5">
+                        <div class=" h-24 w-24 rounded-l-2xl bg-slate-100"></div>
+                    </div>
+                    
                 </div>
                 <div class="bg-blue-100">sidebar</div>
             </div>
