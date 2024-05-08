42da256635a51c21d0fbfa1d12f6254a2dc2e0d5
waarrdii <waarrdii@gmail.com>
2024-05-08 15:16:51 +0700

    sidebar fix for lg screen

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,20 +1,19 @@
 <template>
-    <div class="flex flex-col h-screen">
-            <div class="flex h-12 text-gray-50 bg-cyan-500 w-36">
-                <Logo/>
+    <div class="flex relative flex-col h-screen">
+            <div class="flex h-12 text-gray-50 bg-cyan-500 w-36 lg:w-44">
+                <div class="absolute flex top-4 left-2">
+                    <Logo/>
                 <span>Dashboard</span>
-            </div>
-            <div class="flex-1 bg-slate-100 ">
-                <div class="h-14 relative overflow-hidden">
-                    <div class="bg-slate-100">
-                        <div class="absolute bg-cyan-500 w-[100px] h-11 rounded-br-lg"></div>
-                    </div>
-                    <div class="bg-cyan-500 absolute -right-5">
-                        <div class=" h-24 w-16 rounded-l-[40px] bg-slate-100"></div>
-                    </div>
-                    
+
                 </div>
-                <div class="bg-blue-100">sidebar</div>
+            </div>
+            <div class="flex border-r border-slate-200 lg:h-5">
+                <div class="w-24 bg-cyan-500 rounded-br-[50%] rounded-bl-[60%]"></div>
+                <div class="bg-cyan-500 flex-1">
+                    <div class="bg-white w-full h-full rounded-tl-[60%]"></div>
+                </div>
+            </div>
+            <div class="flex flex-1 border-r border-slate-200 ">
             </div>
     </div>
 
