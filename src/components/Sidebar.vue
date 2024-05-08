37bb9c692642d40538465bd63e09136169341331
waarrdii <waarrdii@gmail.com>
2024-05-08 13:35:34 +0700

    title bar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="flex flex-col h-screen">
-            <div class="flex h-12 text-gray-50 bg-cyan-500 w-40">
+            <div class="flex h-12 text-gray-50 bg-cyan-500 w-36">
                 <Logo/>
                 <span>Dashboard</span>
             </div>
             <div class="flex-1 bg-slate-100 ">
                 <div class="h-14 relative overflow-hidden">
                     <div class="bg-slate-100">
-                        <div class="absolute -left-[5px] -top-20 h-32 w-32 rounded-full bg-cyan-500"></div>
+                        <div class="absolute bg-cyan-500 w-[100px] h-11 rounded-br-lg"></div>
                     </div>
                     <div class="bg-cyan-500 absolute -right-5">
-                        <div class=" h-24 w-16 rounded-l-2xl bg-slate-100"></div>
+                        <div class=" h-24 w-16 rounded-l-[40px] bg-slate-100"></div>
                     </div>
                     
                 </div>
