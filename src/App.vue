9297825eb7a15d0d5c2605cec9b4d6341a139e8a
waarrdii <waarrdii@gmail.com>
2024-05-18 16:37:24 +0700

    tinggi main content belum memenuhi sisa tinggi screen

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex box-border">
     <Sidebar/>
-    <div class="flex-1">
-      <Navbar/>
-      <router-view class="mt-5"></router-view>
+    <div class="flex-1 ">
+      <div class="flex flex-col">
+        <Navbar/>
+        <router-view class="mt-5"></router-view>
+    </div>
     </div>
   </div>  
 
--- src/views/Pelanggan.vue
+++ src/views/Pelanggan.vue
@@ -1,54 +1,43 @@
 <template>
-    <div>
-        <table class="table-fixed border border-gray-300 rounded-lg mx-auto">
-        <thead class="bg-slate-50">
-            <tr>
-                <th class="border border-r-gray-300  px-2">No</th>
-                <th class="border border-r-gray-300  px-2">First Name</th>
-                <th class="border border-r-gray-300  px-2">Last Name</th>
-                <th class="border border-r-gray-300  px-2">Email</th>
-                <th class="border border-r-gray-300  px-2">Company</th>
-                <th class="border border-r-gray-300  px-2">Country</th>
-            </tr>
-        </thead>
-        <tbody class="">
-            <tr v-for="customer, id in displayItems" :key="id" class="border border-b-gray-300">
-                <td class="border border-r-gray-300 px-2">
+    <div class="flex">
+        <div class="mx-auto border rounded-md px-4 py-2">
+        <header class="flex h-8 items-center shadow-sm border-b border-gray-300">
+            <div class="w-8">No</div>
+            <div class="w-24">First Name</div>
+            <div class="w-24">Last Name</div>
+            <div class="w-80">Email</div>
+            <div class="w-80">Company</div>
+            <div class="w-80">Country</div>
+        </header>
+        <main class="h-80 overflow-y-scroll no-scrollbar">
+            <div class="flex" v-for="customer, id in dataCustomers" :key="id">
+                <div class="w-8 border-b border-gray-300">
                     {{ customer.id }}
-                </td>
-                <td class="border border-r-gray-300  px-2">
+                </div>
+                <div class="w-24 border-b border-gray-300">
                     {{ customer.first }}
-                </td>
-                <td class="border border-r-gray-300  px-2">
+                </div>
+                <div class="w-24 border-b border-gray-300">
                     {{ customer.last }}
-                </td>
-                <td class="border border-r-gray-300  px-2">
+                </div>
+                <div class="w-80 border-b border-gray-300">
                     {{ customer.email }}
-                </td>
-                <td class="border border-r-gray-300  px-2 ">
+                </div>
+                <div class="w-80 border-b border-gray-300">
                     {{ customer.company }}
-                </td>
-                <td class=" px-2">
+                </div>
+                <div class="w-80 border-b border-gray-300">
                     {{ customer.country }}
-                </td>
-            </tr>
-        </tbody>
-    </table>
-
-    <div class="flex justify-center">
-        <button 
-        v-for="page, i in totalPages"
-        :key="i"
-        @click.prevent="goToPage(page)"
-        class="mt-4 mx-1 text-gray-50 rounded-full w-8 h-8 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
-        {{ page }}
-    </button>
+                </div>
+            </div>
+        </main>
     </div>
-    
     </div>
-    
 
-<!-- Pagination -->
+
+
+
+    <!-- Pagination -->
 
 </template>
 
@@ -57,9 +46,6 @@ import axios from 'axios';
 import { ref, onMounted, computed } from 'vue';
 
 const dataCustomers = ref(null)
-const currentPage = ref(0)
-const itemsPerPage = ref(10)
-let totalPages = ref(0)
 
 onMounted(async () => {
     try {
@@ -68,25 +54,9 @@ onMounted(async () => {
     } catch (error) {
         console.error('Error fetching data:', error);
     }
-    totalPages = computed(() => {
-        return Math.ceil(dataCustomers.value.length / itemsPerPage.value);
-    });
-    
+
 
 });
-
-const displayItems = computed(() => {
-    if (dataCustomers.value) {
-        return dataCustomers.value.slice(
-            currentPage.value * itemsPerPage.value,
-            (currentPage.value * itemsPerPage.value) + itemsPerPage.value
-        );
-    }
-})
-
-const goToPage = (page)=> {
-  currentPage.value = page - 1; // Convert to 0-based index
-}
 
 </script>
 
