c700d89708be7175c3de171addfafdf19372066f
waarrdii <waarrdii@gmail.com>
2024-05-27 17:48:39 +0700

    date filter

--- src/components/FilterForm.vue
+++ src/components/FilterForm.vue
@@ -1,14 +1,13 @@
 <template>
-    <form class="bg-slate-100 rounded-lg w-80 h-96">
-        <div class="flex justify-betwen items-center">
+    <form class="bg-slate-100 rounded-lg">
+        <div class="flex justify-betwen items-center px-2">
             <input class="m-2 rounded-md border border-slate-200 shadow-sm" type="text" name="" id="" placeholder="Search User">
-            <button class="bg-blue-600 rounded-md px-2 text-white">search</button>
-        </div>
-        <details>
-            
-        </details>
-
-        
+            <button class="bg-blue-600 rounded-md px-2 text-white"><img src="/icons/search.svg" alt="search user"></button>
+            <div class="relative">
+                <div class="flex cursor-pointer select-none"><img src="/icons/filter.svg" alt="filter"> <span>Filter</span></div>
+                <div class="absolute h-20 w-40 bg-slate-100 border-slate-200 border mt-3 rounded">test</div>
+            </div>
+        </div>       
     </form>
 </template>
 
